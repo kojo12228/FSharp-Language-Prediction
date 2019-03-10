0ba0a546e3d9af32edfa53b049da5609f6656a64
Kojo Amoasi <kojo12331@hotmail.co.uk>
2019-03-10 21:09:20 +0000

    create mlcontext and read data

---
+++ LanguageSuggestionML/LanguageParser.fs
@@ -0,0 +1,41 @@
+﻿module LanguageParser
+
+open FSharp.Data
+
+//let fileLocation = @"survey_results_public.csv"
+
+type Survey = CsvProvider<"survey_results_public.csv">
+
+//type LanguageRating(userID, languageID, label) =
+//    new() = LanguageRating(0,0,0)
+//    member x.userID = userID
+//    member x.languageID = languageID
+//    member x.Label = label
+
+type LanguageRating =
+    { userID: int
+      languageID: int
+      Label: int }
+
+
+let survey = new Survey()
+
+let idLanguages =
+    let languages =
+        seq { for row in survey.Rows do
+              yield! row.LanguageWorkedWith.Split(";") }
+        |> Set.ofSeq
+        |> Set.toList
+        |> List.sort
+        |> List.mapi (fun i lang -> (i, lang))
+    languages
+
+let userLanguageRatings =
+    seq { for row in survey.Rows do
+          if row.LanguageWorkedWith <> "NA" then
+              let userLanguages = row.LanguageWorkedWith.Split(";")
+              for id, lang in idLanguages do
+                  yield 
+                      if Array.contains lang userLanguages
+                      then {userID = row.Respondent; languageID = id; Label = 1}
+                      else {userID = row.Respondent; languageID = id; Label = 0} }
--- LanguageSuggestionML/Program.fs
+++ LanguageSuggestionML/Program.fs
@@ -2,7 +2,24 @@
 
 open System
 
+open Microsoft.ML
+
+open LanguageParser
+open Microsoft.ML
+open Microsoft.ML.Data
+
+let trainModel () =
+    let mlContext = new MLContext()
+    let data = mlContext.Data.LoadFromEnumerable(userLanguageRatings)
+    //data.Schema |> printfn "%A"
+    let dataProcessingPipeline =
+        EstimatorChain()
+            .Append(mlContext.Transforms.Conversion.MapValueToKey("userIDEncoded", inputColumnName = "userID"))
+            .Append(mlContext.Transforms.Conversion.MapValueToKey("languageIDEncoded", inputColumnName = "languageID") :> IEstimator<Transforms.ValueToKeyMappingTransformer>)
+    ()
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    //printfn "Hello World from F#!"
+    trainModel()
+    idLanguages |> printfn "%A"
     0 // return an integer exit code
