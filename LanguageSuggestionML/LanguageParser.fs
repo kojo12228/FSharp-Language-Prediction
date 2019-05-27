7d3df2d597abae680b0609a9011927241b9e78c9
Kojo Amoasi <kojo12331@hotmail.co.uk>
2019-05-27 15:55:21 +0100

    Change project from using ML.NET matrix factorization to Math.NET Numerics SVD

--- LanguageSuggestionML/LanguageParser.fs
+++ LanguageSuggestionML/LanguageParser.fs
@@ -1,6 +1,7 @@
 ﻿module LanguageParser
 
 open FSharp.Data
+open MathNet.Numerics.LinearAlgebra
 
 //let fileLocation = @"survey_results_public.csv"
 
@@ -17,6 +18,7 @@ type LanguageRating =
       languageID: int
       Label: int }
 
+let users = 1000
 
 let survey = new Survey()
 
@@ -30,12 +32,57 @@ let idLanguages =
         |> List.mapi (fun i lang -> (i, lang))
     languages
 
+let userIDMap =
+    seq { for row in survey.Rows do
+            if row.Respondent < users then
+                if row.LanguageDesireNextYear <> "NA" then
+                    yield row.Respondent }
+    |> Seq.mapi (fun i x -> (x, i))
+    |> Map.ofSeq
+
 let userLanguageRatings =
     seq { for row in survey.Rows do
-          if row.LanguageWorkedWith <> "NA" then
-              let userLanguages = row.LanguageWorkedWith.Split(";")
-              for id, lang in idLanguages do
-                  yield 
-                      if Array.contains lang userLanguages
-                      then {userID = row.Respondent; languageID = id; Label = 1}
-                      else {userID = row.Respondent; languageID = id; Label = 0} }
+          if row.Respondent < users then
+              if row.LanguageDesireNextYear <> "NA" then
+                  let userLanguages = row.LanguageDesireNextYear.Split(";")
+                  for id, lang in idLanguages do
+                      yield 
+                          if Array.contains lang userLanguages
+                          then {userID = row.Respondent; languageID = id; Label = 1}
+                          else {userID = row.Respondent; languageID = id; Label = 0} }
+
+let userLanguageMap() =
+    userLanguageRatings
+    |> Seq.groupBy (fun x -> x.languageID)
+    |> Seq.map (fun (lID, ratings) ->
+        lID, (ratings |> Seq.filter (fun x -> x.Label = 1) |> Seq.map (fun x -> x.userID)))
+    |> Map.ofSeq
+
+let ratingMatrix =
+    let groupedULR =
+        userLanguageRatings
+        |> Seq.groupBy (fun x -> x.userID)
+    let ratingsVector ratings =
+        [ for x in ratings -> x.Label |> float ]
+    [ for _, userRatings in groupedULR -> ratingsVector userRatings ]
+    |> matrix
+
+let svd =
+    for i, row in Matrix.toRowSeqi ratingMatrix do
+        if i > 70 && i < 80 then
+            printfn "%A" row
+    
+    ratingMatrix
+    |> Matrix.svd
+
+let decomposedRatingMatrix =
+    let sigmaMatrix =
+        svd.W
+        |> Matrix.mapi (fun i j x -> if i = j && i < 20 then x else 0.)
+    svd.U * sigmaMatrix * svd.VT
+
+let predictRating userID languageID =
+    decomposedRatingMatrix.[userIDMap.[userID], languageID]
+
+let predictUserRatings userID =
+    decomposedRatingMatrix.Row(userIDMap.[userID])
--- LanguageSuggestionML/Program.fs
+++ LanguageSuggestionML/Program.fs
@@ -5,21 +5,35 @@ open System
 open Microsoft.ML
 
 open LanguageParser
-open Microsoft.ML
-open Microsoft.ML.Data
+open MathNet.Numerics.LinearAlgebra
 
-let trainModel () =
-    let mlContext = new MLContext()
-    let data = mlContext.Data.LoadFromEnumerable(userLanguageRatings)
-    //data.Schema |> printfn "%A"
-    let dataProcessingPipeline =
-        EstimatorChain()
-            .Append(mlContext.Transforms.Conversion.MapValueToKey("userIDEncoded", inputColumnName = "userID"))
-            .Append(mlContext.Transforms.Conversion.MapValueToKey("languageIDEncoded", inputColumnName = "languageID") :> IEstimator<Transforms.ValueToKeyMappingTransformer>)
-    ()
 [<EntryPoint>]
 let main argv =
     //printfn "Hello World from F#!"
-    trainModel()
+    //trainModel()
+    //idLanguages |> printfn "%A"
     idLanguages |> printfn "%A"
+
+    //let x = svd
+    //x.S.Count |> printfn "%A"
+
+    //predictRating 8 3 |> printfn "C# Prediction: %f"
+    //for i in predictUserRatings 8 do
+    //    printfn "%f" i
+
+    decomposedRatingMatrix.RowCount
+    |> printfn "%d"
+
+    let languageMap = userLanguageMap()
+    let languageAverage =
+        [ for userID in languageMap.[16] ->
+          predictUserRatings userID |> Vector.toList ]
+        |> matrix
+        |> Matrix.toColSeq
+        |> Seq.map (Seq.average)
+        |> Seq.zip idLanguages
+    for x in languageAverage do
+        printfn "%A" x
+
+
     0 // return an integer exit code
